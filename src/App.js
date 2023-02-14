2ec8ff1dd3c84b9876cbe6d9edfbe03fa53577a8
Nikitha <nikkishetty2002@gmail.com>
2023-02-14 10:57:02 +0530

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import Image from "./assets/hi.jpg";
+
+function App() {
+  var text;
+  const x=10;
+  if(x === 10){
+  text="equal";
+  }
+  else{
+    text="not equal";
+  }
+      
+  return <div className="App"><h1>hello world</h1>
+  <p>this is paragraph </p>
+  <p>{5+55}</p>
+  <img  src={Image} alt="imfg" width="200px"/>
+  <h2>{text}</h2>
+  <h2>{x >= 10 ? "greater":"lesser"}</h2>
+  </div>;
+      
+}
+
+export default App;
